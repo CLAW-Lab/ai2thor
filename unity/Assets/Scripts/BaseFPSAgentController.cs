5f8c6858abd6cba99702698cfa144a898a6f5156
Raghav Kapoor <raghavkapoor16@hotmail.com>
2022-10-30 14:44:55 -0400

    Customized Random Seed for object mass

--- unity/Assets/Scripts/BaseFPSAgentController.cs
+++ unity/Assets/Scripts/BaseFPSAgentController.cs
@@ -709,10 +709,10 @@ namespace UnityStandardAssets.Characters.FirstPerson {
         }
 
         public void randomizeObjectMass() {
+            int customSeed = 10302022;
+            Random.InitState(customSeed);
             foreach (var sop in GameObject.FindObjectsOfType<SimObjPhysics>()) {
                 Rigidbody rb = sop.GetComponent<Rigidbody>();
-                int customSeed = 10302022;
-                Random.InitState(customSeed);
                 rb.mass = rb.mass * UnityEngine.Random.Range(0.5f, 1.5f);  // change at most 50% of the mass
             }
         }
